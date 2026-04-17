0fc9489c004ea9c775880eb1bbea8eaeb579d511
hflocki <heiko.flocke@gmail.com>
2026-04-17 21:41:16 +0200

    Update __init__.py

--- custom_components/meteoalarm/__init__.py
+++ custom_components/meteoalarm/__init__.py
@@ -7,7 +7,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
-from .const import BASE_URL, CONF_API_KEY, DOMAIN, SCAN_INTERVAL_MINUTES
+from .const import BASE_URL, CONF_API_KEY, CONF_COUNTRY, DOMAIN, SCAN_INTERVAL_MINUTES
 
 _LOGGER = logging.getLogger(__name__)
 PLATFORMS = ["sensor"]
